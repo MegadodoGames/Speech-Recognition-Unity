eb2b877c7f35a62f58766c12ba3c47d648136777
Chanchana Sornsoontorn <off9955555@gmail.com>
2018-07-16 15:21:17 +0700

    Add PlayerMenuVoiceSystem

---
+++ SpeechRecognitionUnity/Assets/Scripts/PlayerMenuVoiceSystem.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class PlayerMenuVoiceSystem : MonoBehaviour
+{
+    private SpeechRecognitionEngine speechEngine;
+    public Text results;
+
+    // Use this for initialization
+    private void Start()
+    {
+        speechEngine = GetComponent<SpeechRecognitionEngine>();
+        speechEngine.onPhraseRecognized += OnCommandReceived;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        speechEngine.Listen();
+    }
+
+    private void OnCommandReceived(PhraseRecognizedEventArgs args)
+    {
+        string word = args.text;
+        results.text = string.Format("You said: <b>{0}</b>\nConfidence: {1}\nPhrase Duration: {2}",
+            word,
+            args.confidence,
+            args.phraseDuration);
+        print(results.text);
+    }
+}
--- SpeechRecognitionUnity/Assets/Scripts/SpeechRecognitionEngine.cs
+++ SpeechRecognitionUnity/Assets/Scripts/SpeechRecognitionEngine.cs
@@ -1,14 +1,29 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Windows.Speech;
 
+/*
+ * This class is designed to be called by other classes
+ * Attach this component to a game object
+ * Then use onSpeechRecognized += to bind your method
+ * Then call Listen() to start the engine.
+ * Once a phrase is recognized, your method will be invoked and the engine will stop.
+ * If you want a new phrase, you will need to call Listen() again.
+ */
 public class SpeechRecognitionEngine : MonoBehaviour
 {
     public string[] keywords = {"up", "down", "left", "right"};
     public ConfidenceLevel confidence = ConfidenceLevel.Medium;
-    public Text results;
-    protected PhraseRecognizer recognizer;
-    protected string word = "right";
+    public PhraseRecognizer.PhraseRecognizedDelegate onPhraseRecognized;
+    private PhraseRecognizer recognizer;
+
+    /*
+     * Listen once for a phrase and then stop listening
+     */
+    public void Listen()
+    {
+        if (!recognizer.IsRunning)
+            recognizer.Start();
+    }
 
     private void Start()
     {
@@ -16,27 +31,22 @@ public class SpeechRecognitionEngine : MonoBehaviour
         {
             recognizer = new KeywordRecognizer(keywords, confidence);
             recognizer.OnPhraseRecognized += OnPhraseRecognized;
-            recognizer.Start();
         }
     }
 
     private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
     {
-        word = args.text;
-        results.text = "You said: <b>" + word + "</b>";
-        print(results.text);
-    }
-
-    private void Update()
-    {
+        onPhraseRecognized.Invoke(args);
+        recognizer.Stop();
     }
 
     private void OnApplicationQuit()
     {
-        if (recognizer != null && recognizer.IsRunning)
+        if (recognizer != null)
         {
             recognizer.OnPhraseRecognized -= OnPhraseRecognized;
             recognizer.Stop();
+            recognizer.Dispose();
         }
     }
 }
