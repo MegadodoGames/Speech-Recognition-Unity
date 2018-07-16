a322714722d6fa158c694cf3823cbb54a2a0c0a4
Chanchana Sornsoontorn <off9955555@gmail.com>
2018-07-16 13:57:00 +0700

    Remove the bee and its gameplay

--- SpeechRecognitionUnity/Assets/Scripts/SpeechRecognitionEngine.cs
+++ SpeechRecognitionUnity/Assets/Scripts/SpeechRecognitionEngine.cs
@@ -6,11 +6,7 @@ public class SpeechRecognitionEngine : MonoBehaviour
 {
     public string[] keywords = {"up", "down", "left", "right"};
     public ConfidenceLevel confidence = ConfidenceLevel.Medium;
-    public float speed = 1;
-
     public Text results;
-    public Image target;
-
     protected PhraseRecognizer recognizer;
     protected string word = "right";
 
@@ -19,46 +15,27 @@ public class SpeechRecognitionEngine : MonoBehaviour
         if (keywords != null)
         {
             recognizer = new KeywordRecognizer(keywords, confidence);
-            recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+            recognizer.OnPhraseRecognized += OnPhraseRecognized;
             recognizer.Start();
         }
     }
 
-    private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
     {
         word = args.text;
         results.text = "You said: <b>" + word + "</b>";
+        print(results.text);
     }
 
     private void Update()
     {
-        float x = target.transform.position.x;
-        float y = target.transform.position.y;
-
-        switch (word)
-        {
-            case "up":
-                y += speed;
-                break;
-            case "down":
-                y -= speed;
-                break;
-            case "left":
-                x -= speed;
-                break;
-            case "right":
-                x += speed;
-                break;
-        }
-
-        target.transform.position = new Vector3(x, y, 0);
     }
 
     private void OnApplicationQuit()
     {
         if (recognizer != null && recognizer.IsRunning)
         {
-            recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
+            recognizer.OnPhraseRecognized -= OnPhraseRecognized;
             recognizer.Stop();
         }
     }
